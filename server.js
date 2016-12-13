a0ef5eab992109c73162f29256696babcc572216
Philip <toycampani@gmail.com>
2016-12-13 10:44:07 +0800

    Heroku deploy

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', function(req, res){
 	res.render('index.html');
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log('Listening to port 3000...')
 })
