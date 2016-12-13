583cc4b0b43e595ba5c5a10b78fa8a6a879ead84
Philip <toycampani@gmail.com>
2016-12-13 10:02:43 +0800

    Added Server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static('public'));
+app.use(express.static('vendor'));
+
+app.get('/', function(req, res){
+	res.render('index.html');
+});
+
+app.listen(3000, function(){
+	console.log('Listening to port 3000...')
+})
